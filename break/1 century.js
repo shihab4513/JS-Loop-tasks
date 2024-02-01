4b37699c568bf470285cda657659a3800830a35f
shihab4513 <shihab4513@gmail.com>
2024-02-01 19:19:44 +0600

    array loop task added

--- break/1 century.js
+++ break/1 century.js
@@ -3,3 +3,11 @@
 Write a loop 1 to 200. Use break to exit the loop once you find 100.
 
 */
+const array = [1, 2, 3, 4, 5, 6, 7];
+
+for (let i = 1; i < array.length; i++) {
+ if (i == 5) {
+  continue;
+ }
+ console.log(array[i]);
+}
